717ab16612d7b917f8ad619ea27bbeaefc517d5c
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2022-11-07 12:02:26 -0600

    codigo comentado añadido

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -13,26 +13,27 @@ class ConfirmPasswordController extends Controller
     | Confirm Password Controller
     |--------------------------------------------------------------------------
     |
-    | This controller is responsible for handling password confirmations and
-    | uses a simple trait to include the behavior. You're free to explore
-    | this trait and override any functions that require customization.
+    | Este controlador es responsable de manejar las confirmaciones de contraseña y
+    | utiliza un rasgo simple para incluir el comportamiento. Eres libre de explorar
+    | este rasgo y anula cualquier función que requiera personalización.
     |
     */
 
     use ConfirmsPasswords;
 
     /**
-     * Where to redirect users when the intended url fails.
+     * Va a redirigir a los usuarios a la pantalla HOME después de iniciar sesión.
      *
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
-     * Create a new controller instance.
+     * Crear nueva instancia del controlador
      *
      * @return void
      */
+    // Consturctor donde usamos un middleware con usuario autenticado
     public function __construct()
     {
         $this->middleware('auth');
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,26 +13,27 @@ class LoginController extends Controller
     | Login Controller
     |--------------------------------------------------------------------------
     |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
+    | Este controlador maneja la autenticación de los usuarios para la aplicación y
+    | redirigirlos a su pantalla de inicio. El controlador usa un rasgo
+    | para proporcionar convenientemente su funcionalidad a sus aplicaciones.
     |
     */
 
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * Va a redirigir a los usuarios a la pantalla HOME después de iniciar sesión.
      *
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
-     * Create a new controller instance.
+     * Crear nueva instancia del controlador
      *
      * @return void
      */
+    // Consturctor donde usamos un middleware con invitado
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
