75c0b41759de7de989ed7781f66ca6953c7118cd
cheloBalboa18 <marcegol10@outlook.com>
2022-11-14 22:14:01 -0600

    Chelo 14/11/2022
    error en queryController

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -7,18 +7,27 @@ use Illuminate\Support\Facades\DB;
 class QueryController extends Controller
 {
     public function loadQuery(Request $request)
-    {
-        if ($request->ajax()) {
-            $query = DB::table('students')->join('student_data_semester', 'student_data_semester.student_id', '=', 'students.id')
-            ->join('data', 'data.id', '=', 'student_data_semester.data_id');
+    {     
+        //   $query = DB::table('students')->join('student_data_semester', 'student_data_semester.student_id', '=', 'students.id')
+        //  ->join('data', 'data.id', '=', 'student_data_semester.data_id')
+        //  ->join('careers','careers.id', "=", "students.career_id")
+        //  ->select('students.*','careers.name as career','data.*','student_data_semester.*')
+        //  ->get();
 
+        //  dd($query);
+        if ($request->ajax()) {
+            $query = DB::table('students')
+        ->join('student_data_semester', 'student_data_semester.student_id', '=', 'students.id')
+        ->join('data', 'data.id', '=', 'student_data_semester.data_id')
+        ->join('careers','careers.id', "=", "students.career_id")
+        ->select('students.*','careers.name as career','data.*','student_data_semester.*');
+            
             if($request->semester_id)//check if specify semester
                 $query->where('student_data_semester.semester_id', '=', $request->semester_id);
             if($request->career)//check if specify career
-                $query->where('students.career', '=',$request->career);
+                $query->where('careers.name', '=',$request->career);
             if($request->generation)//check if specify generation
-                $query->where('students.generation', '=',$request->generation);
-            
+                $query->where('student.generation', '=',$request->generation);
             return $query->get();
         }
     }
